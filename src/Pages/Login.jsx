628218015d61094a81f0e831cd86788ee5dc5b89
Alsaef <saef.ratul@gmail.com>
2025-05-22 08:05:11 +0600

    dev

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,13 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import GoogleLoginButton from '../Components/GoogleLoginButton ';
 import { toast } from 'react-toastify';
 import { AuthContext } from '../Provider/AuthProvider';
-
+import { FiEye, FiEyeOff } from 'react-icons/fi';
 const Login = () => {
     const {LoginUser}=useContext(AuthContext)
     const navigate = useNavigate()
+    const [showPassword, setShowPassword] = useState(false);
     const handleLogin = (e) => {
         e.preventDefault();
 
@@ -42,11 +43,20 @@ const Login = () => {
                             <input type="email" name='email' placeholder="Enter your email" className="input input-bordered" />
                         </div>
 
-                        <div className="form-control mb-4">
+                        <div className="form-control mb-4 relative">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" name='password' placeholder="Enter your password" className="input input-bordered" />
+                            <input type={showPassword ? 'text' : 'password'} name='password' placeholder="Enter your password" className="input input-bordered" />
+
+                             <button
+                                                        type="button"
+                                                        tabIndex={-1}
+                                                        className="absolute right-3 bottom-0 z-30 -translate-y-1/2 text-gray-500 hover:text-blue-600"
+                                                        onClick={() => setShowPassword((prev) => !prev)}
+                                                    >
+                                                        {showPassword ? <FiEyeOff size={20} /> : <FiEye size={20} />}
+                                                    </button>
                         </div>
 
                         <button type="submit" className="btn btn-primary w-full">
